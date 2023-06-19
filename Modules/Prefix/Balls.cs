13c9488570ef2b949fcea8943cdf8f3b30e0c8b6
Vidak Nedeljković <vidak.nedeljkovic@outlook.com>
2023-06-19 15:40:07 +0200

    New command

---
+++ Modules/Prefix/Balls.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W_bot.Modules.Prefix
+{
+    public class Balls : ModuleBase<SocketCommandContext>
+    {
+        [RequireNsfw]
+        [Command("balls")]
+        public async Task BallSend() => await Context.Channel.SendFileAsync("Media\\balls.png");
+    }   
+}
